44ce29de894a4a9968e680fa31fb6999c44da14c
winwinchit1355 <winwinchit1355@gmail.com>
2023-12-05 00:09:30 +0630

    createStore second parameter

--- src/redux/reducers.jsx
+++ src/redux/reducers.jsx
@@ -1,26 +1,13 @@
 import { combineReducers } from 'redux';
 
-const getDbProduct = () =>{
-    let dbData = localStorage.getItem('products');
-    return dbData ? JSON.parse(dbData):[];
-}
-const saveDbProduct = (products) =>{
-    localStorage.setItem('products',JSON.stringify(products));
-}
-const productReducer = (state = getDbProduct(),{type,payload}) =>{
+const productReducer = (state = [],{type,payload}) =>{
     switch(type){
         case "add":
-            saveDbProduct(payload);
-            state = getDbProduct();
+            state = payload;
             return state;
         case "remove":
-            state = getDbProduct();
-            let pds = state.filter(item => item.id != payload);
-            saveDbProduct(pds);
-            state =  getDbProduct()
-            return state;
+            return state.filter(item => item.id != payload);
         default:
-            state = getDbProduct();
             return state;
     }
 }
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -5,13 +5,13 @@ import thunk from 'redux-thunk';
 const data = {
     products:[
         {
-            'key' : 'mgmg',
+            'id' : 'mgmg',
             'title':'Mg Mg',
             'price':'111',
             'image':"https://th.bing.com/th/id/OIP.mlYbdF2yJKHWy3qm3wXqPQHaE8?rs=1&pid=ImgDetMain"
         },
         {
-            'key' : 'aungaung',
+            'id' : 'aungaung',
             'title':'Aung Aung',
             'price':'111',
             'image':"https://th.bing.com/th/id/OIP.mlYbdF2yJKHWy3qm3wXqPQHaE8?rs=1&pid=ImgDetMain"
